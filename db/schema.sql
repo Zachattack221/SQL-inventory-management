4518dc984a505dacfb1d40277bb1db5e7c801b97
Zachattack221 <zsvd09@gmail.com>
2023-03-16 17:28:49 -0400

    installed sequelize, updated schema to create/use inventory_db, updates to folders

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
-/* TODO: drop if exists, establish db, use db, establish table structure  */
+/* TODO: use db, establish table structure  */
+-- DROP DATABASE
+DROP DATABASE IF EXISTS inventory_db;
+
+-- CREATE DATABASE
+CREATE DATABASE inventory_db;
+
+-- USE DATABASE
+USE inventory_db;
+
+-- TODO: CREATE TABLES
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // TODO: set up connection to server, isolate to individual connection.js page
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const ctable = require('console.table');
 const express = require('express');
+// const ctable = 
+require('console.table');
 
 const routes = require('./routes');
