f0fb899158caca3d7a8641cbc0618381241b4854
Zachattack221 <zsvd09@gmail.com>
2023-03-22 15:33:10 -0400

    index.js notated for models category, dependant upon specific inventory, will update

---
+++ models/index.js
@@ -0,0 +1,7 @@
+// import models
+
+// Define the relationships between models
+
+// list each relationship, one-to-many, one-to-one, etc. Include foreign key relationship and onDelete: 'CASCADE'
+
+// export models for use in C.R.U.D. operation queries
